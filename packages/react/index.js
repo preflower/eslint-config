26f8efcc2100fc7e40ba997af5a6e10488febd56
Preflower <preflower.lin@gmail.com>
2022-01-13 16:10:29 +0800

    chore: remove not detect react warning

--- packages/react/index.js
+++ packages/react/index.js
@@ -2,5 +2,10 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     '@preflower/eslint-config-typescript'
-  ]
+  ],
+  settings: {
+    react: {
+      version: '17.0'
+    }
+  }
 }

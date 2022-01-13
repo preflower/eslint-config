f9b0863b7218061ac5da79163f46efe8659badeb
Preflower <preflower.lin@gmail.com>
2022-01-13 16:14:16 +0800

    fix: typescript failed error

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -3,7 +3,19 @@ module.exports = {
     '@preflower/eslint-config-basic',
     'standard-with-typescript'
   ],
-  rules: {
-    '@typescript-eslint/no-non-null-assertion': 'off'
-  }
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      rules: {
+        // TS
+        '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
+        // Override JS
+        indent: 'off',
+        '@typescript-eslint/indent': ['error', 2],
+        // Off
+        '@typescript-eslint/no-non-null-assertion': 'off'
+      }
+    }
+  ]
 }
